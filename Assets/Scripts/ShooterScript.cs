7aea115356a73af5560fc73c09fd738df2f0c8df
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-07-14 23:30:26 +0200

    mesadze

---
+++ Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    public float angle = 90.0f;
+    public float shootingRate = 0.2f; // how long it waits between shooting two balls
+    public Transform ball;
+
+    private int state;
+    void Start()
+    {
+        state = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && state == 0){
+            state = 1;
+            float MouseX = Input.mousePosition.x/Screen.width-.5f;
+            float MouseY = Input.mousePosition.y/Screen.width-(8.0f/9.0f);
+            angle = Mathf.Atan(MouseY/MouseX);
+            if(MouseX < 0.0f){
+                angle += Mathf.PI;
+            }
+        }
+
+        if(!Input.GetMouseButton(0) && state == 1){
+            Instantiate(ball, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            state = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public GameObject player;
+    public float Velocity = 5.0f;
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+
+        float AngleInRad = player.GetComponent<ShooterScript>().angle;
+        rb.AddForce(new Vector3(Velocity*Mathf.Cos(AngleInRad), Velocity*Mathf.Sin(AngleInRad), 0.0f), ForceMode2D.Force);
+    }
+
+    void Update(){
+        if(transform.position.y < -4.5f){
+            Destroy(gameObject);
+        }
+    }
+
+}
