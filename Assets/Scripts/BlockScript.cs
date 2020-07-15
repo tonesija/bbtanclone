0caee2c3588608fb77ff8d0595dc4da2fb90efd7
Ivan Vlahov <ivan.vlahov@gmail.com>
2020-07-15 17:57:07 +0200

    ajmoooo

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class BlockScript : MonoBehaviour
+{
+    private int health;
+    private TextMeshPro tm;
+    
+    void Start()
+    {
+        health = 20;
+        tm = GetComponentInChildren<TextMeshPro>();
+        tm.SetText(health + "");
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other) {
+        
+        health--;
+        
+        if(health == 0){
+            Destroy(this.gameObject);
+        }
+        
+        tm.SetText(health.ToString());
+    }
+
+    /// <summary>
+    /// Should be called after Instantiating the block.
+    /// </summary>
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    
+
+}
---
+++ Assets/Scripts/GridableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridableObject : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+    public void moveDown(){
+        rb.velocity = new Vector2(0f, -1f);
+    }
+
+    public void stop(){
+        rb.velocity = new Vector2(0f, 0f);
+    }
+}
